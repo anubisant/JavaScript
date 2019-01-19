15b8c1d63ad00cc31e224f5e47c68a479da98479
unknown <karthiky7cs1212@gmail.com>
2019-01-19 09:10:55 -0500

    Updated Module Pattern

--- ModulePatterns.js
+++ ModulePatterns.js
@@ -1,22 +1,29 @@
 //Private Properties should not be directly accessed they should be encapsulated
 //Private properities or methods are provided access through public methods
 
+//Revealing Module Pattern
+
 var myModule = (function() {
     'use strict';
  
     var _privateProperty = 'Hello World';
-     
+    var publicProperty = 'I am a public property';
+  
     function _privateMethod() {
         console.log(_privateProperty);
     }
+  
+  	function publicMethod() {
+    	_privateMethod();
+  	}
      
     return {
-        publicMethod: function() {
-            _privateMethod();
-        }
+        publicMethod: publicMethod,
+        publicProperty: publicProperty
     };
 }());
   
-myModule.publicMethod();                    // outputs 'Hello World'   
+myModule.publicMethod();    		        // outputs 'Hello World'   
+console.log(myModule.publicProperty);       // outputs 'I am a public property'
 console.log(myModule._privateProperty);     // is undefined protected by the module closure
 myModule._privateMethod();                  // is TypeError protected by the module closure
